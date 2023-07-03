a3369a28a4defc90aa321cd3cba2bb1336a64d8b
André Valim Pelz <51752448+andrepelz@users.noreply.github.com>
2023-07-03 19:53:59 -0300

    Add files via upload

---
+++ src/components/topbar/Topbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <biblioteca-navbar>
+    <template #right>
+      <div class="d-flex align-items--center">
+        <biblioteca-button @click="onUsuarios">
+          Perfil
+        </biblioteca-button>
+        <biblioteca-button @click="onLogout">
+          Sair
+        </biblioteca-button>
+      </div>
+    </template>
+  </biblioteca-navbar>
+</template>
+
+<script>
+import { removeCookie } from '@/helpers/cookies/cookie';
+import { goToLoginPage } from '@/router/route.service';
+import { goToLivros, goToEmprestimos, goToUsuarios } from '@/modules/gerenciar/gerenciar.routes';
+
+export default {
+  name: 'BibliotecaTopbar',
+  data() {
+    return {
+    };
+  },
+  methods: {
+    removeCookie,
+    onLogout() {
+      removeCookie('session_id');
+      goToLoginPage();
+    },
+    onLivros() {
+      goToLivros(this.$router);
+    },
+    onEmprestimos() {
+      goToEmprestimos(this.$router);
+    },
+    onUsuarios() {
+      goToUsuarios(this.$router);
+    },
+  },
+};
+</script>
